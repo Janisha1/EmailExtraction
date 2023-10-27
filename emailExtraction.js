40308010445ece3d07043e74f97c563e9f222f3b
1janisha <1janisha@gmail.com>
2023-10-27 23:10:23 +0100

    Part 2 - Using Regex

--- emailExtraction.js
+++ emailExtraction.js
@@ -1,12 +1,7 @@
 const fs = require("fs");
 const file = fs.readFileSync("test.txt", "utf-8");
 
-let counter = 0;
-
-for(let i=0; i<file.length; i++){
-  if(file.substring(i, i+14) === "@softwire.com " || file.substring(i, i+15) === "@softwire.com\r\n") {
-    counter++;
-  }
-}
-
-console.log(counter);
+  /* 2nd Exercise - using RegEx */
+  const regex = /\S+(@softwire\.com)\b/g;
+  const matches = [...file.matchAll(regex)];
+  console.log(`Emails found with "@softwire.com:" ${matches.length}`);
